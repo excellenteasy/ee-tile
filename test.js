6a923a1af27d1f2ce361998dc1424123dedc4296
David Pfahler <david@excellenteasy.com>
2014-12-28 17:30:05 +0100

    test: inital

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 var test = require('tape')
 var component = require('./')
 
-test('component directive', function (t) {
+test('component pattern', function (t) {
   var ddo = component.directive()
   t.plan(6)
   t.equal('function', typeof component.directive, 'directive is of type function')
@@ -13,8 +13,25 @@ test('component directive', function (t) {
   t.ok(ddo.template, 'directive has template')
 })
 
-test('component controller', function (t) {
-  var ctrl = component.controller
-  t.plan(1)
-  t.equal('function', typeof component.directive, 'controller is of type function')
+test('getMainStyles', function (t) {
+  t.plan(3)
+  t.deepEqual(component.getMainStyles(), {})
+  t.deepEqual(component.getMainStyles({
+    background: 'red'
+  }), {background: 'red'})
+  t.deepEqual(component.getMainStyles({
+    background: 'red',
+    image: '../cover.jpg'
+  }), {
+    background: 'red',
+    backgroundImage: 'url(../cover.jpg)',
+    backgroundSize: 'cover'
+  })
+})
+
+test('getBarStyles', function (t) {
+  t.plan(3)
+  t.deepEqual(component.getBarStyles(), {})
+  t.deepEqual(component.getBarStyles({bar: false}), {})
+  t.deepEqual(component.getBarStyles({bar: 'red'}), {background: 'red'})
 })
